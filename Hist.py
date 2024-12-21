357e058635c91384325f9f200ec1d5a789350583
daniilgorin <daniilgorin580@gmail.com>
2024-12-21 11:43:48 +0400

    Все недочеты были учтены и исправлены

--- Hist.py
+++ Hist.py
@@ -3,31 +3,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def get_hist(img: np.ndarray)->np.ndarray:
+def get_hist(img: np.ndarray)->list:
     """
     Получает гистограмму для заданного изображения
     :param img: само изображение
     :return: гистограмма по данному изображению, которая будет использована в show_hist()
     """
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return cv2.calcHist(gray_img, [0], None, [256], [0, 256])
+    channels = cv2.split(img)
+    histograms = []
+
+    for channel in channels:
+        hist = cv2.calcHist([channel], [0], None, [256], [0,256])
+        histograms.append(hist)
+
+    return histograms
 
 
-def show_hist(hist: np.ndarray)->None:
+def show_hist(hist: np.ndarray) -> None:
     """
-    Выводит на экран готовую гистограмму
-    :param hist: гистограмма, полученная в функции get_hist()
+    Выводит на экран гистограмму для каждого канала изображения
+    :param hist: гистограммы по каждому каналу
     """
+    colors = ['red', 'green', 'blue']
     plt.figure(figsize=(10,5))
 
-    plt.title('Гистограмма изображения')
-    plt.xlabel('Интенсивность пикселей')
-    plt.ylabel('Частота')
-    plt.xlim([0, 256])
+    plt.title("Гистограмма изображения")
+    plt.xlabel("Интенсивность пикселей")
+    plt.ylabel("Частота")
 
+    for i, hist in enumerate(hist):
+        plt.plot(hist, color=colors[i], label=f'{colors[i].capitalize()} канал')
+        plt.fill_between(range(256), hist.flatten(), color=colors[i], alpha = 0.5)
 
-    plt.plot(hist, color="black")
-    plt.fill_between(range(256), hist.flatten(), color='black', alpha=0.5)
     plt.grid()
-
+    plt.legend()
     plt.show()
